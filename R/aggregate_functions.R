06a49ef9e1c704c878f8a1fd89f1e6e0a8b58330
Alexander-Morin <alexm.14@outlook.com>
2024-09-12 21:18:06 -0700

    add allrank_mat()

--- R/aggregate_functions.R
+++ R/aggregate_functions.R
@@ -1,4 +1,4 @@
-#' Rank the columns of a matrix
+#' Rank the columns of a matrix such that rank=1 is the highest positive value
 #'
 #' @param mat A numeric matrix
 #' @param ties_arg A character indicating how ties are assigned, as fed into
@@ -13,5 +13,28 @@
 colrank_mat <- function(mat, ties_arg = "min", na_arg = "keep") {
 
   rank_mat <- apply(-mat, 2, rank, ties.method = ties_arg, na.last = na_arg)
+  return(rank_mat)
+}
+
+
+
+
+#' Rank the entire matrix jointly such that rank=1 is the highest positive value
+#'
+#' @param mat A numeric matrix
+#' @param ties_arg A character indicating how ties are assigned, as fed into
+#' the ties argument of the base rank() function. "min" is the assumed default.
+#' @param na_arg A character indicating how NAs are handled, as fed into
+#' the na.last argument of the base rank() function. "keep" is the assumed default.
+#'
+#' @return Return a matrix of integer ranks the same dimension as input matrix
+#' @export
+#'
+#' @examples
+allrank_mat <- function(mat, ties_arg = "min", na_arg = "keep") {
+
+  rank_mat <- rank(-mat, ties.method = ties_arg, na.last = na_arg)
+  rank_mat <- matrix(rank_mat, nrow = nrow(mat), ncol = ncol(mat))
+  rownames(rank_mat) <- colnames(rank_mat) <- rownames(mat)
   return(rank_mat)
 }
--- tests/testthat/test_aggregate_functions.R
+++ tests/testthat/test_aggregate_functions.R
@@ -1,5 +1,5 @@
 # Test for basic functionality
-test_that("colrank_mat() ranks columns correctly", {
+test_that("colrank_mat ranks columns correctly", {
 
   mock_data <- matrix(c(3, 1, 4, 2, 5, 9, 8, 6, 7), nrow = 3, ncol = 3)
   expected_output <- matrix(c(2, 3, 1, 3, 2, 1, 1, 3, 2), nrow = 3, ncol = 3)
@@ -11,7 +11,7 @@ test_that("colrank_mat() ranks columns correctly", {
 
 
 # Test for handling NA values
-test_that("colrank_mat() handles NA values correctly", {
+test_that("colrank_mat handles NA values correctly", {
 
   mock_data <- matrix(c(3, NA, 4, 2, 5, NA, 8, 6, 7), nrow = 3, ncol = 3)
   expected_output <- matrix(c(2, NA, 1, 2, 1, NA, 1, 3, 2), nrow = 3, ncol = 3)
@@ -28,6 +28,42 @@ test_that("colrank_mat assigns ties to minimum values", {
   mock_data <- matrix(c(2, 0, 0, -1, 3, 1, 4, 3, 3, 3, 4, 4), nrow = 4, ncol = 3)
   expected_output <- matrix(c(1, 2, 2, 4, 2, 4, 1, 2, 3, 3, 1, 1), nrow = 4, ncol = 3)
   result <- colrank_mat(mock_data)
+
+  expect_equal(result, expected_output)
+})
+
+
+
+# Test for basic functionality
+test_that("allrank_mat ranks columns correctly", {
+
+  mock_data <- matrix(c(3, 1, 4, 2, 5, 9, 8, 6, 7), nrow = 3, ncol = 3)
+  expected_output <- matrix(c(7, 9, 6, 8, 5, 1, 2, 4, 3), nrow = 3, ncol = 3)
+  result <- allrank_mat(mock_data)
+
+  expect_equal(result, expected_output)
+})
+
+
+
+# Test for handling NA values
+test_that("allrank_mat handles NA values correctly", {
+
+  mock_data <- matrix(c(3, NA, 4, 2, 5, NA, 8, 6, 7), nrow = 3, ncol = 3)
+  expected_output <- matrix(c(6, NA, 5, 7, 4, NA, 1, 3, 2), nrow = 3, ncol = 3)
+  result <- allrank_mat(mock_data)
+
+  expect_equal(result, expected_output)
+})
+
+
+
+# Test for assigning ties
+test_that("allrank_mat assigns ties to minimum values", {
+
+  mock_data <- matrix(c(-1, 0, 6, 3, 0, 4, -2, 0, 4), nrow = 3, ncol = 3)
+  expected_output <- matrix(c(8, 5, 1, 4, 5, 2, 9, 5, 2), nrow = 3, ncol = 3)
+  result <- allrank_mat(mock_data)
 
   expect_equal(result, expected_output)
 })
